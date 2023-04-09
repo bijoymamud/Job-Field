51634d19f1ae56b18861cfc8e5781c8a1450ce51
bijoymamud <bijoymamud.09@gmail.com>
2023-04-10 01:28:05 +0600

    added banner section

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -9,14 +9,14 @@ const Navbar = () => {
                 <h3 className='text-4xl font-mono font-bold'>JobHub</h3>
             </div>
 
-            <div>
+            <div className='flex justify-center items-center gap-10 font-bold'>
                 <Link to="/">Home</Link>
                 <Link to="/statistics">Statistics</Link>
                 <Link to="/appliedjob">Applied Jobs</Link>
                 <Link to="/blog">Blog</Link>
             </div>
             <div>
-                <button className='btn-apply'>Star Applying</button>
+                <button className='btn-apply p-3 rounded-lg'>Star Applying</button>
             </div>
 
         </nav>
---
+++ src/components/SingleJob/SingleJob.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SingleJob = () => {
+    return (
+        <div>
+            <h3>This is for single job card</h3>
+        </div>
+    );
+};
+
+export default SingleJob;
